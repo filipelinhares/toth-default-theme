9af2e7dcfd972a6582e5cac67ab062d0da3652c3
Filipe Linhares <lip.linhares@gmail.com>
2016-05-07 18:37:07 -0300

    New theme
    - Prevent cssurl and jsurl to repeat empty
    - Change theme color
    - Improves mobile experience

--- js/toth.js
+++ js/toth.js
@@ -1,8 +1,6 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.toth-loader').classList.remove('is-open');
-
   var codes = document.querySelectorAll('pre code');
   [].forEach.call(codes, function (code) {
     hljs.highlightBlock(code);
